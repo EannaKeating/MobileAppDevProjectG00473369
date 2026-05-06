ca7efc434115adda094b344c00b001136b605c65
Eanna Keating <G00473369@atu.ie>
2026-05-06 22:15:43 +0000

    G00473369: Initial HomePage with header and Favourites button

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {Router} from '@angular/core'; // Need to add router for the favourites page
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonButtons } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonButtons],
 })
 export class HomePage {
-  constructor() {}
+  constructor(private router: Router) {} // Constructor for navigating pages, runs when HomePage is created
+
+  goToFavourites() {
+    this.router.navigate(['favourites']); // function to navigte to Favourites page
+  }
 }
