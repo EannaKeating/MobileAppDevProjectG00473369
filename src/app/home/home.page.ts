111d9570b093f03e8d6e31e83fe437755c063e31
Eanna Keating <G00473369@atu.ie>
2026-05-06 22:42:57 +0000

    G00473369: HomePage header with student number and Favourites button working

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import {Router} from '@angular/core'; // Need to add router for the favourites page
+import {Router} from '@angular/router'; // Need to add router for the favourites page
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonButtons } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+  standalone: true, // makes component self contained so can work witot being declared
   imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonButtons],
 })
 export class HomePage {
