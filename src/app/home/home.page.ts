1d03aab9f07d3ecb09364cd3cf1a2f5865c7bf21
Eanna Keating <G00473369@atu.ie>
2026-05-09 14:27:07 +0000

    trying to get the home page to display movies from site but not working. need to debug and try again

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,18 +1,32 @@
 import { Component } from '@angular/core';
 import {Router} from '@angular/router'; // Need to add router for the favourites page
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonButtons } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonButtons, IonList, IonItem, IonLabel, IonThumbnail } from '@ionic/angular/standalone';
+import { TmdbService } from '../services/tmdb.service';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true, // makes component self contained so can work witot being declared
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonButtons],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonButtons, IonList, IonItem, IonLabel, IonThumbnail],
 })
 export class HomePage {
-  constructor(private router: Router) {} // Constructor for navigating pages, runs when HomePage is created
+  movies: any[] = []; //this array list holds movies
+
+  constructor(private router: Router, private tmdb: TmdbService) {} // Constructor for navigating pages, runs when HomePage is created
+  async ngOnInit(){
+    this.movies = await this.tmdb.getTrendingMovies(); // to load the trending movies on the home page when the page runs
+  }
 
   goToFavourites() {
     this.router.navigate(['favourites']); // function to navigte to Favourites page
   }
+
+ goToDetails(movie: any) {
+  this.router.navigate(['/movie-details', movie.id]); // navigate to movie details page when built
+ }
+ getImage(path: string) {
+  return this.tmdb.getImageUrl(path); // get movie poster URL
+ }
 }
---
+++ src/app/services/tmdb.service.ts
@@ -0,0 +1,21 @@
+import {Injectable} from '@angular/core'; //allows class to be usable through the app
+import axios from 'axios'; //used to call TMDB API to get movie data
+
+const API_KEY = '7309bad0a92d21fbb45442aa76a681c6';// my personal API key
+const BASE_URL = 'https://api.themoviedb.org/3'; //URL for TMDBs API
+
+@Injectable({
+    providedIn: 'root'
+})
+export class TmdbService {
+    constructor() {} 
+
+    async getTrendingMovies() {
+       const res = await axios.get(`${BASE_URL}/trending/movie/day?api_key=${API_KEY}`); //returns results from TMBD
+       return res.data.results;
+    }
+    getImageUrl(path: String, size: string = 'w500') {
+        return path ? `https://image.tmdb.orh/t/p${size}${path}` : null;
+        
+    }
+}
